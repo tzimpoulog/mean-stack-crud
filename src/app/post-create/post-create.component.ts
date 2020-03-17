9cd43999ad28733395579ae7669711f0f885b874
tzimpoulog <tzimpoulasnikos@gmail.com>
2020-03-17 21:19:38 +0200

    Add image upload functionality

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from './../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
@@ -17,10 +17,23 @@ export class PostCreateComponent implements OnInit {
   postId: string;
   post: Post
   isLoading: boolean = false;
+  form: FormGroup;
+  imagePreview: string;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators:[Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators:[Validators.required, Validators.minLength(3)]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')){
         this.mode = 'edit';
@@ -33,7 +46,9 @@ export class PostCreateComponent implements OnInit {
             title: postData.title,
             content: postData.content
           }
-          console.log("onupdt: " + this.post)
+          this.form.setValue({
+            title: this.post.title, 
+            content: this.post.content})
         });
       } 
       else{
@@ -44,18 +59,30 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid)
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid)
       return;
+
     this.isLoading = true;
     if(this.mode === 'create'){
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     }
     else{
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content)
+      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content)
     }
     
-    form.resetForm();
+    this.form.reset();
   }
 
 
